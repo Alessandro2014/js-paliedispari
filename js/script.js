177e4c238ac7897ee6dd60d22ef06dfa057ba610
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-12 17:15:21 +0200

    funzione random

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto
+
+1 chiedere all'utente di scegliere tra pari o dispari
+2 chiedere di scegliere un numero da 1 a 5
+3 generare funzione per numero random da 1 a 5
+4 creare variante somma
+5 creare funzione di somma 
+6 stampare in pagina chi ha vinto*/
+
+var userChoice = prompt("Scegli tra Pari o Dispari", "Pari");
+var userNumber = parseInt(prompt("Scegli un numero da 1 a 5", "2"));
+var random = randomNumber();
+console.log("scelta utente " + userChoice);
+console.log("scelta numero " + userNumber);
+console.log("scelta random " + random);
+
+var sum = userNumber + random;
+console.log(sum);
+
+function randomNumber() {
+    var number = Math.floor(Math.random() * 5) + 1;
+    return number;
+}
